cd598911300da26a7e39db3ad2cf8496dffc367d
unknown <madushan@WistarIT-MDM.Wistar.upenn.edu>
2016-05-06 16:44:16 -0400

    removed commented service function

--- Services/AirTableService.js
+++ Services/AirTableService.js
@@ -1,39 +1,3 @@
-// (function () {
-//     "use strict";   
-//     var AirTableService = function ($http, $q, SecretsService) { 
-//         var AirTableMethods = {
-//             getMyRounds: function(AirTable_secret){                
-//                 var deferObject_myRounds;
-//                 var myRounds_promise = $http.get(AirTable_secret.url+'callback=JSON_CALLBACK', {
-//                     params: {
-//                         view: 'Main View',       
-//                         maxRecords: 10,
-//                         sort: [{"field": 'Score', "direction":'asc'}]                        
-//                     },
-//                     paramSerializer: '$httpParamSerializerJQLike',                    
-//                     headers : {
-//                         'Authorization' : AirTable_secret.apikey,
-//                         'Content-Type' : 'application/json'
-//                     }
-//                 });
-//                 deferObject_myRounds = deferObject_myRounds || $q.defer();
-                
-//                 myRounds_promise.then(function(data){
-//                     deferObject_myRounds.resolve(data);
-//                 });                
-//                 return deferObject_myRounds.promise;                
-//             }
-//         };
-//         return AirTableMethods;
-//     };
-    
-//     AirTableService.$inject = ['$http', '$q', 'SecretsService'];
-    
-//     angular.module('appGolf')
-// 	  .service('AirTableService', AirTableService);
-    
-// }());
-
 (function () {
     "use strict";   
     var AirTableService = function ($http) { 
