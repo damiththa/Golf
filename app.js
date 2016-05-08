028f9265d4167aa69dbf3f654f9ebfd0b209ceb6
Madushan D. Memmendarachchi <damiththa@gmail.com>
2016-05-07 22:32:58 -0400

    Including routes

--- app.js
+++ app.js
@@ -7,6 +7,15 @@
             .when('/', {
                 controller: 'MainController',
                 templateUrl: 'Views/home.html'
+            })
+            .when('/rounds', {
+                templateUrl: 'Views/rounds.html'
+            }) 
+            .when('/courses', {
+                templateUrl: 'Views/courses.html'
+            }) 
+            .when('/about', {
+                templateUrl: 'Views/about.html'
             })            
             .otherwise({redirectTo: '/'});
 	}]);
