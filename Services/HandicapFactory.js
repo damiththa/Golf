daa40fe34f5e8b7c7e04aa6057f71e8564739661
Madushan D. Memmendarachchi <damiththa@gmail.com>
2016-05-07 22:34:22 -0400

    Introducing and using Differentials

--- Services/HandicapFactory.js
+++ Services/HandicapFactory.js
@@ -4,18 +4,42 @@
         
         var factory = {};
 
+        factory.DifferentialsToUse = function(TotalRounds){
+            var DiffsToUse = 0;
+            if(TotalRounds < 6){
+                DiffsToUse = 1;
+            }else if(TotalRounds < 8){
+                DiffsToUse = 2;
+            }else if(TotalRounds < 10){
+                DiffsToUse = 3;
+            }else if(TotalRounds < 12){
+                DiffsToUse = 4;
+            }else if(TotalRounds < 14){
+                DiffsToUse = 5;
+            }else if(TotalRounds < 16){
+                DiffsToUse = 6;
+            }else if(TotalRounds < 17){
+                DiffsToUse = 7;
+            }else if(TotalRounds < 18){
+                DiffsToUse = 8;
+            }else if(TotalRounds < 19){
+                DiffsToUse = 9;
+            }else{
+                DiffsToUse = 10;
+            }            
+            return DiffsToUse;
+        },
         factory.CalcHandicap = function (myrounds) {
-            // console.log(myrounds);                           
+            //Getting Number of Differentials according to number of rounds
+            var Diffs = factory.DifferentialsToUse(myrounds.length);                                    
             var ttl = 0;
-            var RoundsCnt = myrounds.length;
-            for(var i=0; i<RoundsCnt;i++){
+            for(var i=0; i<Diffs; i++){
                 // console.log(Math.ceil(myrounds[i].fields.HandicapDifferential * 100)/100);
                 ttl += Math.ceil(myrounds[i].fields.HandicapDifferential * 100)/100;
-            };
-            
-            var HandicapIndex = (ttl/RoundsCnt)*0.96;            
-            return Math.round(HandicapIndex * 10)/10
-        };
+            };            
+            var HandicapIndex = (ttl/Diffs)*0.96;            
+            return Math.round(HandicapIndex * 10)/10;
+        }
         return factory;
     };  
     
